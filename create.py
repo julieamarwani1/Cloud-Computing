1ec7c700fec0af81ea07503d62a94634adbbc153
max Sonebäck <maxsoneback@hotmail.com>
2020-10-13 20:05:21 +0000

    made create to function

--- create.py
+++ create.py
@@ -6,24 +6,29 @@ from util import get_credentials
 import os
 import yaml 
 
-loader = loading.get_plugin_loader('password')
+def create(stack_name= 'max1'):
+    loader = loading.get_plugin_loader('password')
 
-credentials = get_credentials()
-auth = loader.load_from_options(**credentials)
+    credentials = get_credentials()
+    auth = loader.load_from_options(**credentials)
 
-sess = session.Session(auth=auth)
-heat = client.Client('1', session=sess)
-#for stack in heat.stacks.list():
-#    print(stack)
+    sess = session.Session(auth=auth)
+    heat = client.Client('1', session=sess)
+    for stack in heat.stacks.list():
+        if 'max1' in stack.name.lower():
+            print('stack already exists')
+            return False
 
 
 
-file_path = os.getcwd() + f'/heat.yml'
-if os.path.isfile(file_path):
-    args = yaml.safe_load(open(file_path))
-    args['heat_template_version'] = '2013-05-23'
-else:
-    sys.exit(f"{file_path} is not in current working directory")
+    file_path = os.getcwd() + f'/heat.yml'
+    if os.path.isfile(file_path):
+        args = yaml.safe_load(open(file_path))
+        args['heat_template_version'] = '2013-05-23'
+    else:
+        print(f"{file_path} is not in current working directory")
+        return False
 
-#print(args)
-heat.stacks.create(stack_name='max1', template=args)
+    #print(args)
+    heat.stacks.create(stack_name='max1', template=args)
+    return True
