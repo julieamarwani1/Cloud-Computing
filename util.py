ca645e899a6a97ba3263edac290eeb4a97494609
max Sonebäck <maxsoneback@hotmail.com>
2020-10-14 09:28:21 +0000

    fixing some syntax bugs + adding key

--- util.py
+++ util.py
@@ -4,18 +4,18 @@ from os import environ as env
 
 
 def create_and_validate_cfg_dict(list_of_nodes):
-        """
-        :param list_of_nodes: A list of all nodes that should be created on initialization
-        :return: a dict with the node-name as key and the config file for said node as valueI
-        """
-        file_paths_dict = {}
-        for node in list_of_nodes:
-            file_path = os.getcwd() + f'/{node}-cloud-cfg.txt'
-            if os.path.isfile(file_path):
-                file_paths_dict[node] = open(file_path)
-            else:
-                sys.exit(f"{file_path} is not in current working directory")
-                return file_paths_dict
+    """
+    :param list_of_nodes: A list of all nodes that should be created on initialization
+    :return: a dict with the node-name as key and the config file for said node as valueI
+    """
+    file_paths_dict = {}
+    for node in list_of_nodes:
+        file_path = os.getcwd() + f'/configs/{node}-cloud-cfg.yml'
+        if os.path.isfile(file_path):
+            file_paths_dict[node] = open(file_path)
+        else:
+            sys.exit(f"{file_path} is not in current working directory")
+    return file_paths_dict
             
 def get_credentials():
     """
